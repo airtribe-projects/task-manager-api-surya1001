3de43b13564fd7f7cd41ef602bb6d891f40713e0
surya1001 <suryamaurya10@gmail.com>
2025-11-22 12:57:36 +0530

    Handled task management apis

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+require("dotenv").config();
+const taskRoutes = require("./routes/task.route");
+
 const app = express();
-const port = 3000;
+
+const port = process.env.port;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+app.use("/tasks", taskRoutes);
+
+// Health check route
+app.get("/", (req, res) => {
+    res.send("Task API is running");
 });
 
-
+app.listen(port, (err) => {
+    if (err) return console.log('Something bad happened', err);
+    console.log(`Server is listening on ${port}`);
+});
 
 module.exports = app;
